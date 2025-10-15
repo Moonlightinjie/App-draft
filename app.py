5fb95c012be6a356a43d179cfd1c18e0fbef3f9e
Moonlightinjie <tahlialyssasmith@gmail.com>
2025-10-15 02:19:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ st.set_page_config(page_title=" The Simpler Solar Calculator", layout="centered"
 
 tab1, tab2, tab3 = st.tabs 
 
-
-(The first tab)
 with tab1:
     st. title ("Solar Power Calculator")
     st. write ("Enter the information you know and allow the system to calculate for you")
-(Angle/tilt calculations)
 Wattage = st.number_input ("Panel Wattage (W)", value = 400)
 Latitude = st.number_input ("Input the latitude of your geographical area (google it and then enter it)", value = 90)
 Season = st.selectbox ("What times are you trying to get maximum sunlight absorption for?", ["The whole year", "Summer", "Winter"])
